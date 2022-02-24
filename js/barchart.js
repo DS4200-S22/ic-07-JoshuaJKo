7b910e503ec873ad6a8e79870f0be45189072fbb
JoshuaJKo <ko.j@northeastern.edu>
2022-02-24 12:35:21 -0500

    commented barchart

--- js/barchart.js
+++ js/barchart.js
@@ -9,6 +9,7 @@ Modified: 12/08/21
 // Build your bar charts in this file 
 
 
+
 // Set dimensions and margins for plots 
 const width = 900; 
 const height = 450; 
@@ -17,6 +18,7 @@ const yTooltipOffset = 15;
 
 
 // TODO: What does this code do? 
+// This code creates variable for a indivudal bar with the given paramters 
 const svg1 = d3
   .select("#hard-coded-bar")
   .append("svg")
@@ -42,31 +44,36 @@ const data1 = [
 */ 
 
 // TODO: What does this code do? 
+// Creates a mallable variable which finds the max of data1 and function d, then returns the score of d
 let maxY1 = d3.max(data1, function(d) { return d.score; });
 
-// TODO: What does each line of this code do?   
+// TODO: What does each line of this code do?  
+// Creates a variable that is representative of the scale of the y axis
 let yScale1 = d3.scaleLinear()
             .domain([0,maxY1])
             .range([height-margin.bottom,margin.top]); 
 
 // TODO: What does each line of this code do? 
+// Creates a variable that is representative of the scale of the x axis
 let xScale1 = d3.scaleBand()
             .domain(d3.range(data1.length))
             .range([margin.left, width - margin.right])
             .padding(0.1); 
 
 // TODO: What does each line of this code do?  
+//adds another attribute to the inital shape. Scales the shape according to the y axis
 svg1.append("g")
    .attr("transform", `translate(${margin.left}, 0)`) 
    .call(d3.axisLeft(yScale1)) 
    .attr("font-size", '20px'); 
 
 // TODO: What does each line of this code do? 
+//adds another attribute to the inital shape. Scales the shape according to the x axis
 svg1.append("g")
     .attr("transform", `translate(0,${height - margin.bottom})`) 
     .call(d3.axisBottom(xScale1) 
             .tickFormat(i => data1[i].name))  
-    .attr("font-size", '20px'); 
+    .attr("font-size", '20px');   
 
 /* 
 
@@ -75,6 +82,7 @@ svg1.append("g")
 */
 
 // TODO: What does each line of this code do? 
+//adds another div container to the html with the given attributes.
 const tooltip1 = d3.select("#hard-coded-bar") 
                 .append("div") 
                 .attr('id', "tooltip1") 
@@ -82,18 +90,21 @@ const tooltip1 = d3.select("#hard-coded-bar")
                 .attr("class", "tooltip"); 
 
 // TODO: What does each line of this code do?  
+//creates a function for when a bar is moused over.
 const mouseover1 = function(event, d) {
   tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") 
           .style("opacity", 1);  
 }
 
 // TODO: What does each line of this code do? 
+//displays the current left px and top px of where the mouse is.
 const mousemove1 = function(event, d) {
   tooltip1.style("left", (event.x)+"px") 
           .style("top", (event.y + yTooltipOffset) +"px"); 
 }
 
 // TODO: What does this code do? 
+//sets the opacity of the tooltip to 0 (hides it).
 const mouseleave1 = function(event, d) { 
   tooltip1.style("opacity", 0); 
 }
@@ -105,6 +116,7 @@ const mouseleave1 = function(event, d) {
 */
 
 // TODO: What does each line of this code do? 
+//compiles all bars and adds them with functions and the correct height and width.
 svg1.selectAll(".bar") 
    .data(data1) 
    .enter()  
